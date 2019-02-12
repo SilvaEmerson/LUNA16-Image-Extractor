a0893d2d4a66c94f5a10723f2c1fcd7f9bb2b637
SilvaEmerson <emersonjose877@gmail.com>
2019-02-12 16:15:36 -0300

    Delete comments

--- main.py
+++ main.py
@@ -31,9 +31,6 @@ def main(
         if not os.path.isdir(bin_patient_dir):
             os.mkdir(bin_patient_dir)
 
-        # get candidates of the pacient
-        # io_inst, cands = utils.read_csv(cand_path, patient_id)
-
         Observable.from_(image_arr).map(utils.normalize_planes).zip(
             Observable.range(0, slices_num),
             lambda image, ind: {"image": image, "z_coord": ind},
@@ -44,29 +41,6 @@ def main(
         ).subscribe(
             on_completed=lambda: print("Finished!"), on_error=lambda err: print(err)
         )
-        # z_coord = (
-        #     cands.map(
-        #         lambda cand: np.asarray(
-        #             [float(cand[3]), float(cand[2]), float(cand[1])]
-        #         )
-        #     ).map(get_voxel_coord)
-        #     # get z coord. of a image
-        #     .map(lambda coord: int(coord[0]))
-        # )
-
-        # # get image from z_coord observable
-        # image = z_coord.map(lambda z_coord: utils.normalize_planes(image_arr[z_coord]))
-
-        # # use zip operator to concatenate each image/z_coord pair
-        # save_image = z_coord.zip(
-        #     image, lambda ind, image: {"image": image, "z_coord": ind}
-        # ).map(lambda data: partial(utils.save_scan, patient_id, **data))
-
-        # save_image.tap(lambda fn: fn(output_path=output_path)).tap(
-        #     lambda fn: fn(output_path=bin_output_path, file_format="npy")
-        # ).subscribe(
-        #     on_completed=lambda: io_inst.close(), on_error=lambda err: print(err)
-        # )
 
     return _
 
