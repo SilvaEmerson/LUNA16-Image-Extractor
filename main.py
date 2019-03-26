2be9437b9d40e46a0b650eb9d0e4168cadc2e683
SilvaEmerson <emersonjose877@gmail.com>
2019-03-26 16:41:04 -0300

    Remove useless lines

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import re
 from functools import partial
-from typing import Callable
 
 import numpy as np
 from rx import Observable
@@ -9,59 +8,56 @@ from rx import Observable
 import utils
 
 
-def main(
-    cand_path: str, output_path: str, bin_output_path: str
-) -> Callable[[str], None]:
-    def _main(img_path: str) -> Observable:
-        image_arr, origin, spacing = utils.load_itk_image(img_path)
+def main(img_path: str) -> Observable:
+    image_arr, origin, spacing = utils.load_itk_image(img_path)
 
-        get_voxel_coord = partial(utils.world_to_voxel_coord, origin, spacing)
+    output_path = os.environ['OUTPUT_PATH']
+    cand_path = os.environ['CAND_PATH']
+    bin_output_path = os.environ['BIN_OUTPUT_PATH']
 
-        # number of scans slices
-        slices_num = image_arr.shape[0]
+    get_voxel_coord = partial(utils.world_to_voxel_coord, origin, spacing)
 
-        # capture the pacient id by regexp
-        patient_id = re.findall("^.*\/(.*).mhd$", img_path)[0]
+    # number of scans slices
+    slices_num = image_arr.shape[0]
 
-        candidates_regions = utils.read_csv(cand_path, patient_id)
+    # capture the pacient id by regexp
+    patient_id = re.findall("^.*\/(.*).mhd$", img_path)[0]
+    
+    patient_dir = os.path.join(output_path, patient_id)
+    bin_patient_dir = os.path.join(bin_output_path, patient_id)
 
-        patient_dir = os.path.join(output_path, patient_id)
-        bin_patient_dir = os.path.join(bin_output_path, patient_id)
+    if not os.path.isdir(patient_dir):
+        os.mkdir(patient_dir)
+    if not os.path.isdir(bin_patient_dir):
+        os.mkdir(bin_patient_dir)
 
-        if not os.path.isdir(patient_dir):
-            os.mkdir(patient_dir)
-        if not os.path.isdir(bin_patient_dir):
-            os.mkdir(bin_patient_dir)
-
-        return (
-            Observable.from_(image_arr)
-            .map(utils.normalize_planes)
-            .zip(
-                Observable.range(0, slices_num),
-                lambda image, ind: {"image": image, "z_coord": ind},
-            )
-            .map(lambda data: partial(utils.save_scan, patient_id, **data))
-            .tap(lambda save_at: save_at(output_path=patient_dir))
-            .tap(
-                lambda save_at: save_at(
-                    output_path=bin_patient_dir, file_format="npy"
-                )
+    return (
+        Observable.from_(image_arr)
+        .map(utils.normalize_planes)
+        .zip(
+            Observable.range(0, slices_num),
+            lambda image, ind: {"image": image, "z_coord": ind},
+        )
+        .map(lambda data: partial(utils.save_scan, patient_id, **data))
+        .tap(lambda save_at: save_at(output_path=patient_dir))
+        .tap(
+            lambda save_at: save_at(
+                output_path=bin_patient_dir, file_format="npy"
             )
         )
-
-    return _main
+    )
 
 
 if __name__ == "__main__":
     limit, config = utils.get_running_params()
 
-    INPUT_PATH = config.get("INPUT_PATH")
-    OUTPUT_PATH = config.get("OUTPUT_PATH")
-    CAND_PATH = config.get("CAND_PATH")
-    BIN_OUTPUT_PATH = config.get("BIN_OUTPUT_PATH")
+    Observable.from_(config)\
+            .tap(lambda var: utils.set_environ(var, config[var]))\
+            .subscribe()\
+            .dispose()
 
     # scandir returns a iterator of os.DirEntry
-    with os.scandir(INPUT_PATH) as subset:
+    with os.scandir(os.environ['INPUT_PATH']) as subset:
         sub = (
             Observable.from_(subset)
             .filter(lambda file: file.name.endswith(".mhd"))
@@ -70,7 +66,7 @@ if __name__ == "__main__":
                 # pluck_attr get the path attribute of each emitted object
             )
             .pluck_attr("path")
-            .flat_map(main(CAND_PATH, OUTPUT_PATH, BIN_OUTPUT_PATH))
+            .flat_map(main)
             .subscribe(on_error=lambda err: print(err))
         )
 
